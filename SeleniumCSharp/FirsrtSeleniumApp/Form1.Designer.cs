5e0ec1a227a304073e1aa24761d8c545d67be2c2
septemberEatMe <vitaliiseptember@gmail.com>
2020-01-06 16:12:47 +0200

    add two dir for JAVA, C#

---
+++ SeleniumCSharp/FirsrtSeleniumApp/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace FirsrtSeleniumApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button button2;
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Чекнуть Виски на Розетке";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            button2.Location = new System.Drawing.Point(316, 12);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(75, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Выход";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(9, 78);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(181, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Подгрузить данные из файла";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(291, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 113);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Папус - приложение Версия 0.7(минимум)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ SeleniumCSharp/FirsrtSeleniumApp/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+
+namespace FirsrtSeleniumApp
+{
+    public partial class Form1 : Form
+    {
+        IWebDriver webDriver;
+        string name = null;
+        string pass = null;
+        string url = "http://wwww.rozetka.com.ua";
+
+        public Form1()
+        {
+            InitializeComponent();
+          
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webDriver = new OpenQA.Selenium.Chrome.ChromeDriver();
+            webDriver.Manage().Window.Maximize();
+            webDriver.Navigate().GoToUrl(url);
+
+            IWebElement whiskey = webDriver.FindElement(By.Name("search"));
+            whiskey.SendKeys("Chivas Regal" + OpenQA.Selenium.Keys.Enter);
+
+            Thread.Sleep(1000);
+
+            IWebElement chivas12 = webDriver.FindElement(By.Id("image_item4698680"));
+            Thread.Sleep(4000);
+            chivas12.Click();
+
+            IWebElement findButtonLink = webDriver.FindElement(By.ClassName("detail-buy-btn-wrap"));
+            Thread.Sleep(4000);
+            findButtonLink.Click();
+
+            //IWebElement searchInput = webDriver.FindElement(By.Name("q"));
+            //searchInput.SendKeys(name + pass  + OpenQA.Selenium.Keys.Enter);
+            //webDriver.Close();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            webDriver.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using(StreamReader reader = new StreamReader("d:/name.txt"))
+            {
+                name = reader.ReadLine();
+                pass = reader.ReadLine();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    } 
+}
